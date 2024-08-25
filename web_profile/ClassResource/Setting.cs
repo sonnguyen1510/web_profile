c8f55abc0728d52662f43d4f6dbca618a0ea6ee6
Son Nguyen <95949597+sonnguyen1510@users.noreply.github.com>
2024-08-25 23:55:21 +0700

    Update

---
+++ web_profile/ClassResource/Setting.cs
@@ -0,0 +1,7 @@
+﻿namespace ClassResource
+{
+    public static class Setting
+    {
+        public static string BuildVersion { get; set; } = DateTime.Now.Ticks.ToString();
+    }
+}
---
+++ web_profile/Resource/wwwroot/Script/CustomScript.js
@@ -0,0 +1,19 @@
+﻿window.AnimatedBackgroundImage = function() {
+    console.log("here")
+    // script.js
+    let lastScrollTop = 0; // Keep track of the last scroll position
+    document.querySelector("content#main-content").addEventListener('scroll', function () {
+        let currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        let transformable = document.querySelector('overlay');
+
+        if (currentScrollTop > lastScrollTop) {
+            // Scrolling down
+            transformable.style.transform = `translate(-50%, calc(-50% + ${currentScrollTop}px))`;
+        } else {
+            // Scrolling up
+            transformable.style.transform = `translate(-50%, calc(-50% - ${currentScrollTop}px))`;
+        }
+
+        lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop; // For Mobile or negative scrolling
+    });
+}
